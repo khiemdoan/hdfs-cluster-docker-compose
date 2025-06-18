52ab28d2472c1c5b4a68bdf983c4b583d41b4034
Khiem Doan <doankhiem.crazy@gmail.com>
2025-06-18 17:43:28 +0700

    Add Zookeeper
    Signed-off-by: Khiem Doan <doankhiem.crazy@gmail.com>

--- mkdirs.sh
+++ mkdirs.sh
@@ -5,6 +5,20 @@ set -e
 mkdir -p .data
 sudo chown 1000:1000 -R .data
 
+# Zookeeper
+mkdir -p .data/zookeeper1_data
+sudo chown 1000:1000 -R .data/zookeeper1_data
+mkdir -p .data/zookeeper1_log
+sudo chown 1000:1000 -R .data/zookeeper1_log
+mkdir -p .data/zookeeper2_data
+sudo chown 1000:1000 -R .data/zookeeper2_data
+mkdir -p .data/zookeeper2_log
+sudo chown 1000:1000 -R .data/zookeeper2_log
+mkdir -p .data/zookeeper3_data
+sudo chown 1000:1000 -R .data/zookeeper3_data
+mkdir -p .data/zookeeper3_log
+sudo chown 1000:1000 -R .data/zookeeper3_log
+
 # NameNode
 mkdir -p .data/namenode
 sudo chown 1000:100 -R .data/namenode
